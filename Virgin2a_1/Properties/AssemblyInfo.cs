7c5e3762c20d2fadafe817b4637eb88075f1095a
B5302165 <B5302165@LAP-0104592>
2023-06-13 14:29:42 +0100

    Add project files.

---
+++ Virgin2a_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Virgin2a_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Virgin2a_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("DCE2C27D-72A0-46D0-B769-8420B349D353")]
+[assembly: AssemblyVersion("1.0.0.0")]
